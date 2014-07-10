5fa9f32294fc76176e0974611bc5d7dc44e1ac7a
jbmilgrom <jbmilgrom@gmail.com>
2014-07-10 18:58:29 -0400

    get wrecked griddler, fingers crossed
    - Deleted Max's frankly embarrassing attempts at logging stuff intelligently on Heroku
    
    - Turns out the Sendgrid article is out of date, and you can't just access Griddler::Email#from as a string anymore. Go figure.

--- app/classes/email_processor.rb
+++ app/classes/email_processor.rb
@@ -1,27 +1,12 @@
 class EmailProcessor
-  
-  # attr_accessor :body, :email, :name
   def initialize(email)
-    @my_sane_log_messages = []
-    @my_sane_log_messages.push("Initializing new EmailProcessor")
-    @my_sane_log_messages.push(email.inspect)
-    
     @email = email
-    @my_sane_log_messages.push("Done")
   end
 
   def process
-    
-    @my_sane_log_messages.push("Processing email")
-    @my_sane_log_messages.push(@email.inspect)
   	Post.create!({ 
       body: @email.body, 
-      from: @email.from 
-      })
-    @my_sane_log_messages.push("Done.")
-    puts @my_sane_log_messages
+      from: @email.from[:email]
+    })
   end
-  # def self.process(email)
-  #   Post.create!({ body: email.body, from: email.from })
-  # end
 end
