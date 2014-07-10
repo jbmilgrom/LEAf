20115d58e3ef5c9e2764f9cbc22d1e1f51046048
jbmilgrom <jbmilgrom@gmail.com>
2014-07-10 18:13:08 -0400

    Add some log statements to try diagnosing griddler thing

--- app/classes/email_processor.rb
+++ app/classes/email_processor.rb
@@ -2,17 +2,23 @@ class EmailProcessor
   
   # attr_accessor :body, :email, :name
   def initialize(email)
-    puts email
+    @@email_logger ||= Logger.new("#{Rails.root}/log/email_bs.log")
+    @@email_logger.info("Initializing new EmailProcessor")
+    @@email_logger.info(email.inspect)
+    
     @email = email
+    @@email_logger.info("Done")
   end
 
   def process
-    puts "method invoked"
-    print @email
+    
+    @@email_logger.info("Processing email")
+    @@email_logger.info(@email.inspect)
   	Post.create!({ 
       body: @email.body, 
       from: @email.from 
       })
+    @@email_logger.info("Done.")
   end
   # def self.process(email)
   #   Post.create!({ body: email.body, from: email.from })
