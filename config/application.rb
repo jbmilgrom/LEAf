621c97201eac237ccc8aa01be9f9966973962cbd
jbmilgrom <jbmilgrom@gmail.com>
2014-07-10 11:19:26 -0400

    turning off strong params for testing

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module SocialPocket
   class Application < Rails::Application
     config.autoload_paths << File.join(Rails.root, "lib")
-
+    config.action_controller.permit_all_parameters = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
