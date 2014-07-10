36b824f7a723e2a0a9e4cb678859a7e1f24dae0e
jbmilgrom <jbmilgrom@gmail.com>
2014-07-10 15:59:48 -0400

    iterating on griddle gem

--- app/classes/email_processor.rb
+++ app/classes/email_processor.rb
@@ -1,15 +1,14 @@
 class EmailProcessor
   
-  # for testing show.html.erb
   # attr_accessor :body, :email, :name
- #  def initialize(email)
- #    @email = email
- #  end
-
- #  def process
-	# Post.create!({ body: @email.body, sender: @email.from })
- #  end
-  def self.process(email)
-    Post.create!({ body: email.body, sender: email.from })
+  def initialize(email)
+    @email = email
   end
+
+  def process
+  	Post.create!({ body: @email.body, sender: @email.from })
+  end
+  # def self.process(email)
+  #   Post.create!({ body: email.body, sender: email.from })
+  # end
 end
