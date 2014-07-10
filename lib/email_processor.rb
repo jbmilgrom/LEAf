49d77c77e49df0cc13a83f539b78790584d83986
jbmilgrom <jbmilgrom@gmail.com>
2014-07-10 11:38:25 -0400

    iterating on email_processor

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -1,9 +1,9 @@
 class EmailProcessor
   
   # for testing show.html.erb
-  attr_accessor :body, :email, :name
+  # attr_accessor :body, :email, :name
 
   def self.process(email)
-    Email.create!({ body: email.body, email: email.from })
+    Email.create!({ body: email.body, sender: email.from })
   end
 end
