9a39d088f9f04cd774a7a58610360c81af6ec014
jbmilgrom <jbmilgrom@gmail.com>
2014-07-12 15:29:18 -0400

    add basic article processing methods in ArticleProcessor module using Nokogiri

--- lib/modules/article_processor.rb
+++ lib/modules/article_processor.rb
@@ -1,4 +1,20 @@
 module ArticleProcessor
-	def test_test
+	require 'open-uri'
+
+	def get_doc(url)
+		@doc = Nokogiri.HTML(open(url))
 	end
+
+	def header
+		@article = @doc.xpath("//article//header") if @doc.xpath("//article//header")
+	end
+
+	def print_to_s(ng_object)
+		s = ""
+		ng_object.children.each do |object|
+			s += object.to_s
+		end
+		return s
+	end
+
 end
