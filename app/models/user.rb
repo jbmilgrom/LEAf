99567c15c8ef74628770869f365df908527378be
jbmilgrom <jbmilgrom@gmail.com>
2014-07-13 19:50:01 -0400

    created the follower-followee relationship through User - i.e. many-to-many self-join

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
 	has_many :articles, through: :saved_articles
 	has_many :saved_articles
+
+	has_many :followers, through: :follower_follows, source: :follower, class_name: "Follow"
+	has_many :follower_follows, foreign_key: :followee_id, class_name: "Follow"
+
+	has_many :followees, through: :followee_follows, source: :followee, class_name: "Follow"
+	has_many :followee_follows, foreign_key: :follower_id, class_name: "Follow"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140713023806) do
+ActiveRecord::Schema.define(version: 20140713222619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20140713023806) do
     t.string   "subheader"
     t.integer  "post_id"
     t.text     "header"
+  end
+
+  create_table "follows", force: true do |t|
+    t.integer  "follower_id"
+    t.integer  "followee_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "posts", force: true do |t|
