f388a3aa3491588313b8c52c9e7e904649e70a3f
jbmilgrom <jbmilgrom@gmail.com>
2014-07-14 07:34:24 -0400

    updated follower-followee comments for clarity

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,14 @@ class User < ActiveRecord::Base
 	has_many :articles, through: :saved_articles
 	has_many :saved_articles
 
-	# source :follower matches with the follower belong_to identification in the Follow model 
+	# source: :follower matches with the belong_to :follower identification in the Follow model 
 	has_many :followers, through: :follower_follows, source: :follower, class_name: "Follow"
-	# "name" of Follow joiner table when accessing through the followers associations
+	# follower_follows "names" the Follow joiner table for accessing through the follower association
 	has_many :follower_follows, foreign_key: :followee_id, class_name: "Follow"
 
-	# source :followee matches with the followee belong_to identification in the Follow model 	
+	# source: :followee matches with the belong_to :followee identification in the Follow model 	
 	has_many :followees, through: :followee_follows, source: :followee, class_name: "Follow"
-	# "name" of Follow joiner table when accessing through the followers associations
+	# followee_follows "names" the Follow joiner table for accessing through the followee association
 	has_many :followee_follows, foreign_key: :follower_id, class_name: "Follow"
 
 end
