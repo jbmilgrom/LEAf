77b4683464a7f3bc14c5620fddae21263859d04f
jbmilgrom <jbmilgrom@gmail.com>
2014-07-14 13:21:45 -0400

    refactored sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
 
   get "/log_out" => "sessions#destroy", :as => "log_out"
   get "/log_in" => "sessions#new", :as => "log_in"
-  resources :sessions, except: [:new, :destroy]
-
+  resources :sessions, only: :create
+  
 end
