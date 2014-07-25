ac5662fe1c93bbb6b973072e3331483b0788c2a4
jbmilgrom <jbmilgrom@gmail.com>
2014-07-25 07:14:16 -0700

    added logic in articles index.html to account replace delete button with 'save' where current user != user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,32 +2,37 @@ class ArticlesController < ApplicationController
 
   def index
 
-    # user owner of the saved_articles
-    user = User.find(params[:user_id])
-    # current_user is the viewer of the page
     redirect_to sign_up_path and return unless current_user 
-      if user != current_user
-        redirect_to user_articles_path(current_user.id) and return unless current_user.a_follower?(user)
-      end
-        ####TODO### need to only call @user.update_articles if @user == current_user
-        
-        
-        current_user.update_articles if user == current_user
-        	
-        #### UPDATE DOES NOT WORK QUICK ENOUGH TO GET INTO @SAVED_ARTICLES ON FIRST LOAD	
-    			# @user = User.find(params[:id])
-    			# @user = current_user
-        @articles_with_user = user.articles_with_user
 
-        respond_to do |format|
-    		  format.html # index.html.erb
-          # format.json { binding.pry ; render json: saved_articles }
-    		  format.json { render json: @articles_with_user }
-        end
+    @user = User.find(params[:user_id])
+
+    if @user != current_user
+      redirect_to user_articles_path(current_user.id) and return unless current_user.a_follower?(@user)
+    end
+      ####TODO### need to only call @user.update_articles if @user == current_user
+      
+      
+    @user.update_articles
+    	
+    #### UPDATE DOES NOT WORK QUICK ENOUGH TO GET INTO @SAVED_ARTICLES ON FIRST LOAD	
+
+    @articles_with_user = @user.articles_with_user
+
+    respond_to do |format|
+		  format.html # index.html.erb
+		  format.json { render json: @articles_with_user }
+    end
   end
 
   def destroy
     user = User.find(params[:user_id])
+    binding.pry
+    redirect_to sign_up_path and return unless current_user 
+    ###### TO DO ######
+    ##### THIS is creating a DELETE request to the desired path for some strange reason
+    ##### There is no impact at the moment because the DELETE button has been removed when c_u != user
+    redirect_to user_articles_path(current_user) and return unless current_user == user
+    
     article = Article.find(params[:id])
     saved_article = SavedArticle.where(user_id: user.id, article_id: article.id).first
     post = Post.where({email: user.email, a_url: article.a_url}).first
