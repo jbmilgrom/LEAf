1a58ba9800ee74900ba5885f9cacf26cde2a8d76
jbmilgrom <jbmilgrom@gmail.com>
2014-08-04 19:07:56 -0400

    fix typo in comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ActiveRecord::Base
 
 	# source: :follower matches with the belong_to :follower identification in the Follow model 
 	has_many :followers, through: :follower_follows, source: :follower
-	# follower_follows "names" the Follow joiner table for accessing through the follower association
+	# follower_follows "names" the Follow join table for accessing through the follower association
 	has_many :follower_follows, foreign_key: :followee_id, class_name: "Follow"
 
 	# source: :followee matches with the belong_to :followee identification in the Follow model 	
 	has_many :followees, through: :followee_follows, source: :followee
-	# followee_follows "names" the Follow joiner table for accessing through the followee association
+	# followee_follows "names" the Follow join table for accessing through the followee association
 	has_many :followee_follows, foreign_key: :follower_id, class_name: "Follow"
 
 
