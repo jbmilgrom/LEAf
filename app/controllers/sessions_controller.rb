c3cf8e3dabca5256bfd862fab0a120efd2ee2351
jbmilgrom <jbmilgrom@gmail.com>
2014-08-05 14:01:49 -0400

    fix tabbing bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,19 @@
 class SessionsController < ApplicationController
 
-	def create
-  		user = User.authenticate(params[:email], params[:password])
-  		if user
-    		session[:user_id] = user.id
-    		redirect_to user_articles_path(user.id), :notice => "Logged in!"
-  		else
-    		flash.now.alert = "Invalid email or password"
-    		render "new"
-  		end
-	end
+  def create
+    user = User.authenticate(params[:email], params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect_to user_articles_path(user.id), :notice => "Logged in!"
+    else
+      flash.now.alert = "Invalid email or password"
+      render "new"
+    end
+  end
 
-	def destroy
-  		session[:user_id] = nil
-  		redirect_to root_url, :notice => "Logged out!"
-	end
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, :notice => "Logged out!"
+  end
 	
 end
